bbec9a1a6ce506b27543e3e494d258118f225f8e
Fu Yiqiao <e0970569@u.nus.edu>
2025-03-30 21:57:17 +0800

    Add project structure

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const apiRouter = require('./routes/api');
+const { errorHandler } = require('./middleware/errorHandler');
+
+const app = express();
+app.use(express.json());
+app.use('/api', apiRouter);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
